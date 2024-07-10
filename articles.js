b530c46d4bea459a54da28640216ba73a8859e2d
Aksgo <akshitgoel0412@gmail.com>
2024-07-10 11:32:09 +0530

    "local state maintained"

--- articles.js
+++ articles.js
@@ -1,14 +1,20 @@
-const toggleButton = document.getElementById("toggle-theme")
-const theme = document.getElementById("theme-sheet")
-let darkMode = true;
-toggleButton.addEventListener('click',()=>{
-    darkMode = !darkMode
+const toggleButton = document.getElementById("toggle-theme");
+const theme = document.getElementById("theme-sheet");
+let darkMode = localStorage.getItem('darkMode')==='true';
+const setTheme = ()=>{
     if(darkMode){
-        theme.href="articlestyle.css"
-        toggleButton.innerHTML="🌙"
+        theme.href="articlestyle.css";
+        toggleButton.innerHTML="🌙";
     }
     else{
-        theme.href="articlestylelight.css"
-        toggleButton.innerHTML="☀️"
+        theme.href="articlestylelight.css";
+        toggleButton.innerHTML="☀️";
     }
+}
+setTheme();
+toggleButton.addEventListener('click',()=>{
+    darkMode = !darkMode;
+    //storing theme in local storage to prevent disappearing after reload
+    localStorage.setItem('darkMode', darkMode)
+    setTheme();
 })
