0dff629d3c9f3a3df54962907bdb43cc436bb2a2
Vishal-Gori <vishalgori2208@gmail.com>
2024-10-08 00:32:12 +0530

    added:Code Explaination through commentsn

--- Algorithms/huffman-coding/huffman-coding.cpp
+++ Algorithms/huffman-coding/huffman-coding.cpp
@@ -3,6 +3,9 @@
 #include <unordered_map>
 #include <string>
 
+using namespace std;
+
+// Node of the Huffman Tree
 class HuffmanNode {
 public:
     char character;
@@ -10,9 +13,11 @@ public:
     HuffmanNode* left;
     HuffmanNode* right;
 
+    // Constructor to initialize node
     HuffmanNode(char character, int frequency) : character(character), frequency(frequency), left(nullptr), right(nullptr) {}
 };
 
+// Comparator for priority queue (min-heap)
 struct Compare {
     bool operator()(HuffmanNode* left, HuffmanNode* right) {
         return left->frequency > right->frequency;
@@ -21,11 +26,13 @@ struct Compare {
 
 class HuffmanCoding {
 private:
-    std::unordered_map<char, std::string> huffmanCodes;
+    unordered_map<char, string> huffmanCodes;
 
-    void generateCodes(HuffmanNode* root, const std::string& code) {
+    // Recursive function to generate Huffman codes
+    void generateCodes(HuffmanNode* root, const string& code) {
         if (!root) return;
 
+        // If it's a leaf node, assign the code
         if (!root->left && !root->right) {
             huffmanCodes[root->character] = code;
             return;
@@ -36,13 +43,16 @@ private:
     }
 
 public:
+    // Build Huffman Tree
     HuffmanNode* buildHuffmanTree(char characters[], int frequencies[], int size) {
-        std::priority_queue<HuffmanNode*, std::vector<HuffmanNode*>, Compare> pq;
+        priority_queue<HuffmanNode*, vector<HuffmanNode*>, Compare> pq;
 
+        // Insert all characters into the priority queue
         for (int i = 0; i < size; i++) {
             pq.push(new HuffmanNode(characters[i], frequencies[i]));
         }
 
+        // Combine nodes with the smallest frequencies
         while (pq.size() > 1) {
             HuffmanNode* left = pq.top(); pq.pop();
             HuffmanNode* right = pq.top(); pq.pop();
@@ -52,15 +62,18 @@ public:
             pq.push(newNode);
         }
 
-        return pq.top(); // root of the Huffman tree
+        // Return root of the Huffman Tree
+        return pq.top();
     }
 
+    // Print the Huffman codes
     void printHuffmanCodes() {
         for (const auto& pair : huffmanCodes) {
-            std::cout << pair.first << ": " << pair.second << std::endl;
+            cout << pair.first << ": " << pair.second << endl;
         }
     }
 
+    // Generate Huffman codes
     void generateHuffmanCodes(HuffmanNode* root) {
         generateCodes(root, "");
     }
@@ -71,6 +84,7 @@ int main() {
     int frequencies[] = {5, 9, 12, 13, 16};
     int size = sizeof(frequencies) / sizeof(frequencies[0]);
 
+    // Build and generate Huffman tree and codes
     HuffmanCoding huffman;
     HuffmanNode* root = huffman.buildHuffmanTree(characters, frequencies, size);
     huffman.generateHuffmanCodes(root);
