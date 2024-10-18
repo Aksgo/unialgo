e4a841c0266577f469c048f631fa5fc4fd89cd34
Aksgo <akshitgoel0412@gmail.com>
2024-10-18 16:09:06 +0530

    header animations

--- public/script.js
+++ public/script.js
@@ -73,7 +73,7 @@ searchIcon.addEventListener('click',(e)=>{
 })
 
 const header = document.querySelector('header');
-let lastScrollTop = 50;
+let lastScrollTop = 0;
 
 window.addEventListener('scroll', () => {
     const scrollTop = window.scrollY;
